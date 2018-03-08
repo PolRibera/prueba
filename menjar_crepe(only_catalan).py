2215f92b76a4e3b7a583fb0c32684165295f9422
Pol Ribera <polriberaaymerich@gmail.com>
2018-03-08 21:11:48 +0100

    Añadiendo archivo menjar_crepe(only_catalan)

--- menjar_crepe(only_catalan).py
+++ menjar_crepe(only_catalan).py
@@ -1,9 +1,9 @@
 apetece_crepe_input =input("Vols una crep? (Si/No): ").upper()
 
 if apetece_crepe_input =="SI":
-    apetece_crepe = True
-elif apetece_crepe == "NO"
-    apetece_crepe = False
+     apetece_crepe = True
+elif apetece_crepe == "NO":
+     apetece_crepe = False
 else:
     print("Digues Si o No, com no se que has dit ho compto com no")
     apetece_crepe = False
@@ -14,12 +14,12 @@ esta_creperia_input =input("Esta la creperia? (Si/No): ").upper()
 
 
 
-tiene_dinero =  tiene_dinero_input == "Si"
-estas_avia = estas_avia_input == "si"
+tiene_dinero = tiene_dinero_input == "Si"
+estas_avia = estas_avia_input == "SI"
 
-apetece_crepe = apetece_crepe_input == "Si"
+apetece_crepe = apetece_crepe_input == "SI"
 puede_permitirlo = tiene_dinero or estas_avia
-esta_creperia = esta_creperia_input=="Si"
+esta_creperia = esta_creperia_input=="SI"
 
 
 if apetece_crepe and puede_permitirlo and esta_creperia:
