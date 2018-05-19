ac33bafaeb4d0d44a25db904972b7929a6e1204a
Pol Ribera <polriberaaymerich@gmail.com>
2018-05-19 11:47:32 +0200

    añadiendo README

--- fecha.py
+++ fecha.py
@@ -10,4 +10,4 @@ user_date = datetime.datetime (year=year, month=month, day=day)
 time_remaining = user_date - datetime.datetime.now()
 
 
-print("{} dies,{} hores".format(time_remaining.days, int(time_remaining.seconds / 60 / 60)))
+print("{} dies,{        } hores".format(time_remaining.days, int(time_remaining.seconds / 60 / 60)))
